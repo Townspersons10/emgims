29ef1fc876b4414179444e0d5e3a1bde98f2f1be
Townspersons10 <zerogomez00@gmail.com>
2024-04-03 08:47:36 -0600

    updated app.use

--- backend/index.js
+++ backend/index.js
@@ -17,37 +17,38 @@ const __dirname = dirname(__filename);
 
 // Middleware to log response headers
 app.use((req, res, next) => {
-  const originalSend = res.send;
-  res.send = function (body) {
-    console.log(`Response headers for ${req.path}:`, res.getHeaders());
-    return originalSend.call(this, body);
-  };
-  next();
+    const originalSend = res.send;
+    res.send = function (body) {
+        console.log(`Response headers for ${req.path}:`, res.getHeaders());
+        return originalSend.call(this, body);
+    };
+    next();
 });
 
 console.log(`MongoDB URI: ${process.env.mongoDB_URI}`);
 
 // Attempt to connect to MongoDB
 mongoose.connect(process.env.mongoDB_URI)
-  .then(() => console.log('Successfully connected to MongoDB.'))
-  .catch(err => console.error('Connection error', err));
+    .then(() => console.log('Successfully connected to MongoDB.'))
+    .catch(err => console.error('Connection error', err));
 
 app.use(express.json());
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, "/frontend/dist")));
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '/frontend/dist', 'index.html'));
-  });
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, '/frontend/dist', 'index.html'));
+    });
 }
 
 app.get('/', (req, res) => res.send('Welcome to the Employee Inventory Management System!'));
-app.use('/employee', employeeRoute);
+
+app.use('/employee', express.static(path.join(__dirname,"/employee")));
 
 app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send('Something broke!');
+    console.error(err.stack);
+    res.status(500).send('Something broke!');
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
