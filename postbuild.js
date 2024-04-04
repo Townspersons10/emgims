d8596e05c793c76943d4f75553c33242e64da9b3
Townspersons10 <zerogomez00@gmail.com>
2024-04-03 19:37:38 -0600

    updated postbuild.js file

--- postbuild.js
+++ postbuild.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 
-const sourcePath = path.join(__dirname, '../frontend/dist/index.html');
-const destinationPath = path.join(__dirname, 'frontend/index.html');
+const sourcePath = path.join(__dirname, 'backend/frontend/dist/index.html');
+const destinationPath = path.join(__dirname, 'backend/frontend/index.html');
 
 fs.copyFileSync(sourcePath, destinationPath);
